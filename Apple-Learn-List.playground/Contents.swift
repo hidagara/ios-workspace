96f297390ee8d2b726bad6cbecc3464102bd0025
Roman Guseynov <hidagara@gmail.com>
2020-10-11 18:18:25 +0300

    update project

--- Apple-Learn-List.playground/Contents.swift
+++ Apple-Learn-List.playground/Contents.swift
@@ -1,12 +1,8 @@
 /*:
 # Apple Software Developer Roadmap
  
- * [Apple Wallet (PassKit)](https://developer.apple.com/documentation/passkit)
- * Apple Pay
- * Networking
- * Core Image
- * Core Animations
- * SwiftUI
+ 
+ 
+ 
  
 */
-
